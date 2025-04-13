f11cc533c15fb665f19802f88eed928f3e34e891
macesilveira <bahiacematheus@gmail.com>
2025-04-12 23:57:03 -0300

    na pasta

--- q1.py
+++
@@ -1,2 +0,0 @@
-resto = 10%3
-print (" O resto de 10 por 3 é", resto)
--- q4.py
+++
@@ -1,4 +0,0 @@
-raio_circulo=2
-pi=3.14
-area_circulo=pi*(raio_circulo**2)
-print (" Um circulo com raio de tamanho 2cm tem", area_circulo, "cm")
