8101ba392a657e0f2e87b43b46bfbdf82e75c151
macesilveira <bahiacematheus@gmail.com>
2025-04-12 23:53:10 -0300

    commit da q1 a q4

--- q3.py
+++ q3.py
@@ -1,4 +1,4 @@
 aula_meses=2*16
 pode_faltar = 1/4
 resultado = int(aula_meses*pode_faltar)
-print ("Davinir pode faltar no máximo", resultado, "aulas")
+print (" Davinir pode faltar no máximo", resultado, "aulas")
--- q4.py
+++ q4.py
@@ -1,4 +1,4 @@
 raio_circulo=2
 pi=3.14
 area_circulo=pi*(raio_circulo**2)
-print ("Um circulo com raio de tamanho 2cm tem", area_circulo, "cm")
+print (" Um circulo com raio de tamanho 2cm tem", area_circulo, "cm")
