59d7b7b02ffc67092470efd35861bd2658a310cb
macesilveira <bahiacematheus@gmail.com>
2025-04-26 11:56:13 -0300

    lista002

---
+++ Lista002/q3.py
@@ -0,0 +1,6 @@
+# Questao 3 - Matheus Silveira e Marcos Brandão
+
+nome = input("Digite seu nome: ")
+print('Nome: ', nome,'\nPrimeira letra do nome:', nome[0], '\nÚltima letra do nome:', nome[3],
+      '\nDo primeiro caracter ao quarto:', nome[0:4], '\nQuarto caracter:', nome[3], '\nOs dois últimos',
+      nome[-2:])
