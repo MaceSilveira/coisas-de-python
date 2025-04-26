556d3abc6a366f5eda93fb2eaf9662bebdc339ce
macesilveira <bahiacematheus@gmail.com>
2025-04-26 11:41:01 -0300

    lista002

--- Lista002/q2.py
+++
@@ -1,8 +0,0 @@
-# Questao 2 - Matheus Silveira e Marcos Brandão
-
-num1=float(input("Digite o primeiro numero: "))*1
-num2=float(input("Digite o segundo numero: "))*2
-num3=float(input("Digite o terceiro numero: "))*3
-num4=float(input("Digite o quarto numero: "))*4
-media_ponderada = (num1+num2+num3+num4)/10
-print('A média ponderada dos números digitados é {}'.format(media_ponderada))
