af4134265dfec7a900753d256fdf4636a563dbb1
macesilveira <bahiacematheus@gmail.com>
2025-04-28 22:37:50 -0300

    lista002

--- Lista002/q10.py
+++ Lista002/q10.py
@@ -5,4 +5,4 @@ palavra2= frase.split()[1]
 palavra3= frase.split()[2]
 palavra4= frase.split()[3]
 
-print (f" O tamanho da 1° palavra é {len(palavra1)} letras, da 2° palavra é {len(palavra2)} letra, da 3° palavra é {len(palavra3)} letras, da 4° palavra é {len(palavra4)} letras.")
+print (f"O tamanho da frase é {len(frase)}, 1° palavra é {len(palavra1)} letras, da 2° palavra é {len(palavra2)} letra, da 3° palavra é {len(palavra3)} letras, da 4° palavra é {len(palavra4)} letras.")
